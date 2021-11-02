bb17b55b8a9261ef305d91322488deb177ce91ac
Lorekgaston <lorekgaston@gmail.com>
2021-11-02 16:15:26 -0300

    feat: add react context and global state

---
+++ src/context/alarmscontext.jsx
@@ -0,0 +1,29 @@
+import React, {
+  createContext,
+  useState,
+  useContext,
+} from 'react';
+
+const initialState = {
+  alarms: [],
+  totalAlarmsCount: '',
+  activeAlarmsCount: '',
+};
+
+const useAlarmState = () => useState({ ...initialState });
+
+const AlarmContext = createContext(null);
+
+export const useGlobalState = () => {
+  const value = useContext(AlarmContext);
+  if (value === null) {
+    throw new Error('Please add initialState');
+  }
+  return value;
+};
+
+export const GlobalStateProvider = ({ children }) => (
+  <AlarmContext.Provider value={useAlarmState()}>
+    {children}
+  </AlarmContext.Provider>
+);
